3746cd871a9de48e3bc13b7be79b2f1ccc8a5c97
LauraChristina <laura-christi@hotmail.com>
2016-04-11 18:02:43 -0300

    pi

---
+++ Login.php
@@ -0,0 +1,29 @@
+﻿<?php
+session_start();
+?>
+
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="login.css" />
+</head>
+<body>
+<div class="container">
+	<section id="content">
+		<form action="logado.php" method="post">
+			<h1>Login</h1>
+			<div>
+				<input type="text" placeholder="Username" required id="username" name="username"/>
+			</div>
+			<div>
+				<input type="password" placeholder="Password" required id="password" />
+			</div>
+			<div>
+				<input type="submit" value="Logar" />
+			</div>
+		</form><!-- form -->
+		
+	</section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
---
+++ banco.php
@@ -0,0 +1,12 @@
+﻿<?php
+$dbhost = "koo2dzw5dy.database.windows.net";
+$db = "SenaQuiz";
+$user = "TSI";
+$password = "SistemasInternet123";
+$dsn = "Driver={SQL Server};Server=$dbhost;Port=1433;Database=$db;";
+
+$connect = odbc_connect($dsn,
+						$user,
+						$password);
+$query=("SELECT * FROM Professor");
+?>
